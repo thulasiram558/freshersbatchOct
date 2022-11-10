5d28e0097d16ee1c11a78c801a4640cfba38fefc
thulasiram558 <thulasiram558@gmail.com>
2022-11-10 13:33:27 +0530

    Git remoting demo

---
+++ SampledemoGit/src/com/org/tr/ADDITION.java
@@ -0,0 +1,22 @@
+package com.org.tr;
+
+import java.util.Scanner;
+public class ADDITION 
+{
+	
+	public static void main(String[] args) 
+	{
+		
+		int c;
+		Scanner sc1 = new Scanner(System.in);
+		Scanner sc2 = new Scanner(System.in);
+		System.out.println("The value of a is ");
+		int m=sc1.nextInt();
+		System.out.println("The value of b is ");
+		int n=sc2.nextInt();
+		c=m+n;
+		System.out.println("The result="+c);
+		
+	}
+
+}
